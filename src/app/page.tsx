7985509875d410eba9241da69d79598bfdfd50e6
Mustafa <aslanov7509@gmail.com>
2025-05-26 18:42:56 +0400

    Olivia section is finoshed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/home/Banner/Banner";
 import Products from "@/components/home/Products/Products";
+import Olivia from "@/components/home/Olivia/Olivia";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </section>
 
       <section>
+        <Olivia />
       </section>
     </>
   );
