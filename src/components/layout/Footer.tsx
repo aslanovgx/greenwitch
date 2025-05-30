d570f3694813bc8a0c3279143a4ba5c4f40e6bac
Mustafa <aslanov7509@gmail.com>
2025-05-30 19:35:31 +0400

    Footer section has updated

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,20 +10,20 @@ import Copyright from './../../../public/assets/footer/bx-copyright.svg.svg'
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <div className={`${styles.footerBox} columns flex justify-evenly items-center`}>
-                <ul className='flex flex-col justify-center items-start gap-2'>
+            <div className={`${styles.footerBox} columns flex flex-wrap justify-evenly items-center`}>
+                <ul className='flex flex-col flex-1 max-w-[240px] break-words items-start gap-2 '>
                     <li>Əlaqə</li>
                     <li><span><Phone /></span>+9942002020</li>
                     <li className='relative'><span><Location /></span>Nizami küçəsi 96C, AF Mall, 3-cü mərtəbə, Bakı, Azərbaycan -cü mərtəbə, Bakı, Azərbaycan</li>
                 </ul>
-                <ul className='flex flex-col justify-center items-start gap-2'>
+                <ul className='flex flex-col justify-center items-start gap-2 flex-1 max-w-[240px] break-words'>
                     <li>Haqqımızda</li>
                     <li>Missiyamız</li>
                     <li>Dəyərlərimiz</li>
                     <li>Brendlərimiz</li>
                     <li>Niyə Biz?</li>
                 </ul>
-                <ul className='flex flex-col justify-center items-start gap-2'>
+                <ul className='flex flex-col justify-center items-start gap-2 flex-1 max-w-[240px] break-words'>
                     <li>Məhsullar</li>
                     <li>Yeni gələnlər</li>
                     <li>Ən çox satanlar</li>
@@ -31,7 +31,7 @@ export default function Footer() {
                     <li>Kişi</li>
                     <li>Uşaq</li>
                 </ul>
-                <ul className='flex flex-col justify-center items-start gap-2'>
+                <ul className='flex flex-col justify-center items-start gap-2 flex-1 max-w-[240px] break-words'>
                     <li>Social</li>
                     <li>
                         <span><Facebook /></span>
@@ -43,7 +43,7 @@ export default function Footer() {
             {/* <div className="w-full h-[1px] bg-gradient-to-r from-white via-black to-white" /> */}
             <div className={`${styles.footerGradientLine} mx-auto`}></div>
             <div className={`${styles.greenwich2025} flex justify-center gap-1 items-center`}>
-                <span><Copyright  width={14} height={14} /></span>
+                <span><Copyright width={14} height={14} /></span>
                 <span>Greenwich 2025</span>
             </div>
         </footer>
