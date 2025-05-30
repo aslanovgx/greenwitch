37b56560d492ed39ed3b4db7023cbf6493e0315b
Mustafa <aslanov7509@gmail.com>
2025-05-30 19:06:53 +0400

    Banner section has updated and some fade animations are added

--- src/components/home/Banner/Banner.tsx
+++ src/components/home/Banner/Banner.tsx
@@ -1,7 +1,39 @@
+"use client";
+
+import { useState } from 'react';
 import Image from 'next/image'
 import styles from './Banner.module.css';
+import TextSwitcher from "@/components/ui/TextSwitcher";
+import { motion, AnimatePresence } from "framer-motion";
+
+const bannerSets = [
+    {
+        images: [
+            { src: "/assets/navbar/banner-11.jpg", alt: "banner-11", width: 302 },
+            { src: "/assets/navbar/banner-12.jpg", alt: "banner-12", width: 658 },
+            { src: "/assets/navbar/banner-13.jpg", alt: "banner-13", width: 284 },
+        ],
+    },
+    {
+        images: [
+            { src: "/assets/navbar/banner-21.jpg", alt: "banner-21", width: 302 },
+            { src: "/assets/navbar/banner-22.jpg", alt: "banner-22", width: 658 },
+            { src: "/assets/navbar/banner-23.jpg", alt: "banner-23", width: 284 },
+        ],
+    },
+    {
+        images: [
+            { src: "/assets/navbar/banner-31.png", alt: "banner-31", width: 302 },
+            { src: "/assets/navbar/banner-32.png", alt: "banner-32", width: 658 },
+            { src: "/assets/navbar/banner-33.jpg", alt: "banner-33", width: 284 },
+        ],
+    },
+];
 
 export default function Banner() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    // const [isFading, setIsFading] = useState(false);
+    const currentSet = bannerSets[activeIndex];
     return (
         <>
             <div className={styles.row_3}>
@@ -16,27 +48,39 @@ export default function Banner() {
             </div>
 
             <div className={`${styles.row_4} flex justify-center items-center`}>
-                <Image
-                    src={'/assets/navbar/banner-1.jpg'}
-                    alt='banner-1'
-                    width={302}
-                    height={302}
-                    className={`${styles.banner_1} object-cover`}
+                {currentSet.images.map((img, idx) => (
+                    <motion.div
+                        key={`${activeIndex}-${img.src}`}
+                        initial={{ opacity: 0, scale: 0.98 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        // transition={{ duration: 0.8, ease: "easeInOut" }}
+                        transition={{ type: "spring", stiffness: 60, damping: 12 }}
+                        // transition={{
+                        //     type: "spring",
+                        //     stiffness: 40,
+                        //     damping: 15,
+                        //     mass: 0.8
+                        // }}
+                        className={`relative ${styles[`banner_${idx + 1}`]}`}
+                        style={{ width: img.width, height: 302 }}
+                    >
+                        <Image
+                            src={img.src}
+                            alt={img.alt}
+                            fill
+                            className="object-cover"
+                        />
+                    </motion.div>
+                ))}
+
+                {/* Interval üçün TextSwitcher */}
+                <TextSwitcher
+                    texts={["", "", ""]}
+                    onIndexChange={(i) => {
+                        setActiveIndex(i); // artıq fade effekti Framer Motion-da
+                    }}
                 />
-                <Image
-                    src={'/assets/navbar/banner-2.jpg'}
-                    alt='banner-2'
-                    width={658}
-                    height={302}
-                    className={`${styles.banner_2} object-cover`}
-                />
-                <Image
-                    src={'/assets/navbar/banner-3.jpg'}
-                    alt='banner-3'
-                    width={284}
-                    height={302}
-                    className={`${styles.banner_3} object-cover`}
-                />
+
             </div>
         </>
     );
--- src/components/home/ImageGrid/ImageGrid.tsx
+++ src/components/home/ImageGrid/ImageGrid.tsx
@@ -1,49 +1,114 @@
+"use client";
 import styles from './ImageGrid.module.css';
 import Image from "next/image";
+import TextSwitcher from "@/components/ui/TextSwitcher";
+import { useState } from "react";
+
+const imageSets = [
+  {
+    left: {
+      src: "/assets/home/imageGrid/hilfiger.png",
+      title: "hilfiger",
+    },
+    rightTop: {
+      src: "/assets/home/imageGrid/olivia-black.jpg",
+      title: "olivia burton",
+    },
+    rightBottom: {
+      src: "/assets/home/imageGrid/olivia-pink.jpg",
+      title: "olivia burton",
+    },
+  },
+  {
+    left: {
+      src: "/assets/home/imageGrid/frederique.jpg",
+      title: "frederique Constant",
+    },
+    rightTop: {
+      src: "/assets/home/imageGrid/olivia-pink.jpg",
+      title: "olivia burton",
+    },
+    rightBottom: {
+      src: "/assets/home/imageGrid/swiss.png",
+      title: "swiss-military",
+    },
+  },
+];
 
 export default function ImageGrid() {
-
+  const [activeIndex, setActiveIndex] = useState(0);
+  const currentSet = imageSets[activeIndex];
+  const [isFading, setIsFading] = useState(false);
   return (
 
     <>
       <div className={`${styles.ImageGrid} flex justify-self-center gap-x-[46px]`}>
         <div className={`${styles.leftImage} relative row-span-2`}>
-          <div className={`${styles.img} relative w-[680px] h-[706px]`}>
+          <div
+            className={`${styles.img} relative w-[680px] h-[706px]  transition-opacity duration-700 
+             ${isFading ? "opacity-0" : "opacity-100"
+              }`}
+          >
             <Image
-              src={`/assets/home/imageGrid/hilfiger.png`}
-              alt='hilfiger'
+              key={currentSet.left.src}
+              src={currentSet.left.src}
+              alt={currentSet.left.title}
               fill
               className="object-cover"
             />
           </div >
-          <div className={`${styles.img_desc}`}>
-
+          <div className={`${styles.img_desc}  transition-opacity duration-700
+            ${isFading ? "opacity-0" : "opacity-100"
+            }`}>
+            <span>{currentSet.left.title}</span>
           </div>
         </div>
         <div className={`${styles.rightImage} justify-self-center gap-y-[36px] flex flex-col w-[456px] h-[706px]`}>
-          <div className={`${styles.img} relative w-[456px] h-[341px]`}>
+          <div className={`${styles.img} relative w-[456px] h-[341px]  transition-opacity duration-700 
+          ${isFading ? "opacity-0" : "opacity-100"
+            }`}>
             <Image
-              src={`/assets/home/imageGrid/olivia-black.jpg`}
-              alt='olivia-black'
+              key={currentSet.rightTop.src}
+              src={currentSet.rightTop.src}
+              alt={currentSet.rightTop.title}
               fill
               className="object-cover"
             />
-            <div className={`${styles.img_desc}`}>
-
+            <div className={`${styles.img_desc}  transition-opacity duration-700
+            ${isFading ? "opacity-0" : "opacity-100"
+              }`}>
+              <span>{currentSet.rightTop.title}</span>
             </div>
           </div >
-          <div className={`${styles.img} relative w-[456px] h-[328px]`}>
+          <div className={`${styles.img} relative w-[456px] h-[328px] transition-opacity duration-700 
+          ${isFading ? "opacity-0" : "opacity-100"
+            }`}>
             <Image
-              src={`/assets/home/imageGrid/olivia-pink.jpg`}
-              alt='olivia-pink'
+              key={currentSet.rightBottom.src}
+              src={currentSet.rightBottom.src}
+              alt={currentSet.rightBottom.title}
               fill
               className="object-cover"
             />
-            <div className={`${styles.img_desc}`}>
-
+            <div className={`${styles.img_desc}  transition-opacity duration-700
+            ${isFading ? "opacity-0" : "opacity-100"
+              }`}>
+              <span>{currentSet.rightBottom.title}</span>
             </div>
           </div >
         </div>
+        {/* SWITCHER to auto-change */}
+        <TextSwitcher
+          texts={["", ""]}
+          onIndexChange={(i) => {
+            setIsFading(true); // əvvəlcə gizlət
+            setTimeout(() => {
+              setActiveIndex(i); // sonra şəkli dəyiş
+              setIsFading(false); // sonra görün
+            }, 700); // <-- BU BURADADIR
+          }}
+        />
+
       </div>
     </>
   );
