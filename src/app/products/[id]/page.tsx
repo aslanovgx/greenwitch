f7d7c88af49eb995e970a20fc33b81c025b330fb
Mustafa <aslanov7509@gmail.com>
2025-06-23 16:36:50 +0400

    PageProps is added

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,11 +3,11 @@ import ProductsDetail from './ProductsDetail';
 import mehsullar from '@/components/Mock/Home/mehsullar.json';
 import SimilarProducts from './SimilarProducts';
 import Contact from '@/components/home/Contact/Contact';
-type Props = {
+type PageProps = {
     params: { id: string };
 };
 
-export default function ProductDetailPage({ params }: Props) {
+export default async function ProductDetailPage({ params }: PageProps) {
     const productId = params.id;
     const product = mehsullar.find(item => item.id.toString() === productId);
 
