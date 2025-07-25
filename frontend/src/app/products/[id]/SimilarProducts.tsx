4d60b301b2a4901abf43c92ffdac862c3dc7f611
Mustafa Aslanov <aslanov7509@gmail.com>
2025-07-25 18:50:38 +0400

    .env.local yaradildi

--- frontend/src/app/products/[id]/SimilarProducts.tsx
+++ frontend/src/app/products/[id]/SimilarProducts.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from '@/components/home/MostSales/MostSales.module.css';
 import SectionTitle from '@/components/common/SectionTitle';
 import mehsullar from '@/components/Mock/Home/mehsullar.json';
@@ -8,23 +8,22 @@ import ProductCard from '@/components/common/ProductCard';
 import MoreButton from '@/components/ui/MoreButton';
 
 export default function SimilarProducts() {
-    // const [products, setProducts] = useState<Product[]>([]);
-
-    // useEffect(() => {
-    //     fetch('https://fakestoreapi.com/products')
-    //         .then(res => res.json())
-    //         .then(data => setProducts(data))
-    //         .catch(console.error);
-    // }, []);
+    const [products, setProducts] = useState<Product[]>([]);
     const [activeCardId, setActiveCardId] = useState<number | null>(null);
 
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`)
+            .then(res => res.json())
+            .then(data => setProducts(data))
+            .catch(console.error);
+    }, []);
 
     return (
         <>
             <div className={`${styles.brands}`}>
                 <SectionTitle>Oxşar Məhsullar</SectionTitle>
                 <div className={`${styles.cards_container} flex justify-center items-center`}>
-                    {mehsullar.slice(0,5).map((item: Product) => (
+                    {products.slice(0,5).map((item: Product) => (
                         <ProductCard
                             key={item.id}
                             item={item}
