d0c2e2713f5fbe96744eb12b180f88174da46c87
Mustafa Aslanov <aslanov7509@gmail.com>
2025-07-28 17:08:35 +0400

    IP Address is configured

--- backend/WebAPI/Program.cs
+++ backend/WebAPI/Program.cs
@@ -12,7 +12,7 @@ public class Program
         builder.Services.AddPersistenceServices(builder.Configuration);
         builder.Services.AddApplicationServices();
 
-        // CORS siyas?ti (Next.js v? AdminPanel üçün icaz?)
+        // CORS siyas?ti (Next.js v? AdminPanel ï¿½ï¿½ï¿½n icaz?)
         builder.Services.AddCors(options =>
         {
             options.AddPolicy("AllowAllClients", policy =>
@@ -42,7 +42,7 @@ public class Program
 
         app.UseStaticFiles();
 
-        app.UseHttpsRedirection();
+        // app.UseHttpsRedirection();
 
         // CORS t?tbiqi
         app.UseCors("AllowAllClients");
