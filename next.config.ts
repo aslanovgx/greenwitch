74f15808e51c7563d9d7eff4d9de2c968d594e29
Mustafa Aslanov <aslanov7509@gmail.com>
2025-09-09 01:15:00 +0400

    something is updated

--- next.config.ts
+++ next.config.ts
@@ -3,43 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      // DEV
       {
-        protocol: "https",
-        hostname: "**.ngrok-free.app",
-        pathname: "/**",
-      },
-
-      // PROD (əsas domenin)
-      {
-        protocol: "https",
-        hostname: "saat.az",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.saat.az",
-        pathname: "/**",
-      },
-
-      // Əgər API və ya şəkillər ayrıca hostdandırsa (məs: api.saat.az və ya cdn.saat.az)
-      {
-        protocol: "https",
-        hostname: "api.saat.az",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.saat.az",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: '**.ngrok-free.app',
+        pathname: '/**',
       },
     ],
-    unoptimized: true, // ❌ devdə test üçün true edə bilərsən, amma prod üçün yaxşıdır ki, false qalsın
+    unoptimized: false,
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
     return config;
   },
