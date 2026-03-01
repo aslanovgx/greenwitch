a2a8cefc23dc2bc5e65a24fe754301b36576092e
Mustafa Aslanov <aslanov7509@gmail.com>
2026-03-02 01:59:59 +0400

    menuItems is updated

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -1,11 +1,9 @@
-// src/data/menuItems.ts
-
 const menuItems = [
+  { label: "Saatlar", href: "/products?categoryId=1" },
+  { label: "Aksesuarlar", href: "/products?categoryId=2" },
   { label: "Kişi", href: "/products?Gender=1" },
   { label: "Qadın", href: "/products?Gender=2" },
   { label: "Uşaq", href: "/products?Gender=3" },
-  { label: "Aksesuarlar", href: "/products?categoryId=2" },
-  { label: "Saatlar", href: "/products?categoryId=1" },
   { label: "Mağazalar", href: "/location" },
   { label: "Haqqımızda", href: "/about" },
 ];
