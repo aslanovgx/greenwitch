9cf0d216dbc466fde3006852c3cb8e8a2e5d15a1
Mustafa Aslanov <aslanov7509@gmail.com>
2026-03-02 14:52:17 +0400

    Brand tsx is updated

--- src/components/home/Brands/Brands.tsx
+++ src/components/home/Brands/Brands.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import styles from './Brands.module.css';
 import '@/components/home/Brands/Brands.css'
 import AutoSwiper from '@/components/common/AutoSwiper';
@@ -8,11 +7,11 @@ import SectionTitle from '@/components/common/SectionTitle';
 
 
 const logos = [
-  { image: "/assets/home/brands/frederique.png", alt: "FREDERIQUE CONSTANT", href: "/products?brandId=8" },
-  { image: "/assets/home/brands/armani.png", alt: "EMPORIO ARMANI", href: "/products?brandId=5" },
-  { image: "/assets/home/brands/calvin.png", alt: "CALVIN KLEIN", href: "/products?brandId=6" },
-  { image: "/assets/home/brands/michael.png", alt: "MICHAEL KORS", href: "/products?brandId=1" },
-  { image: "/assets/home/brands/hilfiger.png", alt: "TOMMY HILFIGER", href: "/products?brandId=9" },
+  { image: "/assets/home/brands/frederique.png", alt: "FREDERIQUE CONSTANT", href: "/products?categoryId=1&brandId=8" },
+  { image: "/assets/home/brands/armani.png", alt: "EMPORIO ARMANI", href: "/products?categoryId=1&brandId=5" },
+  { image: "/assets/home/brands/calvin.png", alt: "CALVIN KLEIN", href: "/products?categoryId=1&brandId=6" },
+  { image: "/assets/home/brands/michael.png", alt: "MICHAEL KORS", href: "/products?categoryId=1&brandId=1" },
+  { image: "/assets/home/brands/hilfiger.png", alt: "TOMMY HILFIGER", href: "/products?categoryId=1&brandId=9" },
 ];
 
 export default function Brands() {
